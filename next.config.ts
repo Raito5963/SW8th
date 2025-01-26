b4ace6beb0a50c66170a27aca812788355a41d42
Raito5963 <181489458+Raito5963@users.noreply.github.com>
2025-01-26 00:53:44 +0000

    画像のドメインを設定するためにnext.config.tsを更新

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,10 @@
 const nextConfig = {
   reactStrictMode: true,
 };
-
 module.exports = nextConfig;
-
-
+module.exports = {
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+};
 export default nextConfig;
